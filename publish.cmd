dc55dc5e2c78706476e7891d14dd01d5f26677a0
Moises Tavira DenallixTechEU <moises@denallixtecheu.onmicrosoft.com>
2018-02-16 14:41:21 +0000

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=k2botdemo-Web-Deploy.pubxml -p:Password=ukvGSj2pWixT0bH3dJugv3nppkgjqGvBMbHHHfYaFuo2x4FDLjyad45DHBkn
+
